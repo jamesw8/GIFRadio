7afc26ff3664bc866995e238933a9371c3dad99a
jamesw8 <jameswong8@gmail.com>
2017-10-15 03:08:11 -0400

    submit for testing

--- controllers/gifs.js
+++ controllers/gifs.js
@@ -1,12 +1,34 @@
 const express = require('express');
 const models = require('../models');
+const giphy = require('../giphy');
 
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.json({
-    msg: "Successful GET to '/gifs' route"
-  });
+	let gifs = [];
+	let getGIF;
+
+	var loop = function (resolve, reject) {
+		for (let gif = 0; gif < 20; gif++) {
+	 		getGIF = new Promise((resolve, reject) => {
+				giphy.retrieveGIF(giphy.api_key, resolve, reject)
+			}).then((gif_link) => {
+				gifs.push(gif_link);
+				console.log(gifs);
+			});
+		}
+		resolve(gifs);
+	};
+
+	new Promise((loop, resolve, reject) => {
+		loop(resolve, reject);
+	}).then(() => {
+		res.json({ gifs: gifs });
+	});
+	
+	console.log('Outside',gifs);
+  	res.json({ gifs: ['2','plus','2','is','four','minus','one','that\'s','three',
+  		'quick','maths','ree','eeee','eeeee','eeeee','eeeee','eeeee','eeeee','eeee','eeee']});
 });
 
 module.exports = router;
--- giphy.js
+++ giphy.js
@@ -1,15 +1,14 @@
-// require('es6-promise').polyfill();
 require('isomorphic-fetch');
 fs = require('fs');
 
 let api_key = fs.readFileSync('api_key').toString();
 
-let retrieveGIF = function(api_key) {
+let retrieveGIF = function(api_key, resolve, reject) {
 	fetch('http://api.giphy.com/v1/gifs/random?api_key=' + api_key).then((response) => {
 		return response.json();
 	}).then((json) => {
-		console.log(json['data']['image_original_url']);
-		return json;
+		// console.log(json['data']['image_original_url']);
+		resolve(json['data']['image_original_url']);
 	});
 }
 
