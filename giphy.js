05e56ffc4bb47bb0d61250a56451194ff0ae844a
jamesw8 <jameswong8@gmail.com>
2017-10-15 05:37:53 -0400

    Option to read key from file

--- giphy.js
+++ giphy.js
@@ -1,7 +1,7 @@
 require('isomorphic-fetch');
 fs = require('fs');
 
-let api_key = process.env.giphy_api_key || fs.readFileSync('api_key', 'utf8');
+let api_key = process.env.giphy_api_key || fs.readFileSync('giphy_key', 'utf8');
 
 let retrieveGIF = function(api_key, resolve, reject) {
 	fetch('http://api.giphy.com/v1/gifs/random?api_key=' + api_key).then((response) => {
