9cb3f33b16927b786d2a3c56ab0a0fe6c2d6bf47
erickolbusz <erickolbusz@gmail.com>
2017-10-15 02:58:50 -0400

    stupid bug fix

--- script.js
+++ script.js
@@ -4,17 +4,19 @@ var selected = Array.apply(null, Array(203)).map(Boolean.prototype.valueOf,false
 var clickFcn = function(i) {
   var name = "#cell"+i;
   $(name).click(function() {
+    console.log(count);
     if (selected[i]) {
       $(name).removeClass('select');
       $(name).addClass('not-select');
+      selected[i] = !selected[i];
       count--;
     }
     else if (count<5) {
       $(name).removeClass('not-select');
       $(name).addClass('select');
       count++;
+      selected[i] = !selected[i];
     }
-    selected[i] = !selected[i];
   });
 }
 
