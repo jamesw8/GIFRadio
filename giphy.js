0ebdf1a11cd495e057e96ec65ae48c8e2532e7d5
jamesw8 <jameswong8@gmail.com>
2017-10-15 05:36:02 -0400

    Added option to read from file for key

--- giphy.js
+++ giphy.js
@@ -1,17 +1,18 @@
 require('isomorphic-fetch');
 fs = require('fs');
 
-let api_key = process.env.giphy_api_key;
+let api_key = process.env.giphy_api_key || fs.readFileSync('api_key', 'utf8');
 
 let retrieveGIF = function(api_key, resolve, reject) {
 	fetch('http://api.giphy.com/v1/gifs/random?api_key=' + api_key).then((response) => {
 		return response.json();
 	}).then((json) => {
-		// console.log(json['data']['image_original_url']);
+		// console.log(json['data']);
 		resolve(json['data']['image_original_url']);
 	});
 }
 
+retrieveGIF(api_key);
 module.exports = {
 	api_key: api_key,
 	retrieveGIF: retrieveGIF
