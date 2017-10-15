d870387d93952a1e1a888a7afe2f0402be1a2180
jamesw8 <jameswong8@gmail.com>
2017-10-15 03:37:33 -0400

    Fixed return array of links

--- controllers/gifs.js
+++ controllers/gifs.js
@@ -6,29 +6,21 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
 	let gifs = [];
-	let getGIF;
-
-	var loop = function (resolve, reject) {
-		for (let gif = 0; gif < 20; gif++) {
-	 		getGIF = new Promise((resolve, reject) => {
-				giphy.retrieveGIF(giphy.api_key, resolve, reject)
-			}).then((gif_link) => {
-				gifs.push(gif_link);
-				console.log(gifs);
-			});
-		}
-		resolve(gifs);
-	};
-
-	new Promise((loop, resolve, reject) => {
-		loop(resolve, reject);
-	}).then(() => {
-		res.json({ gifs: gifs });
-	});
+	// let getGIF;
+	let gif_promises = [];
 	
-	console.log('Outside',gifs);
-  	res.json({ gifs: ['2','plus','2','is','four','minus','one','that\'s','three',
-  		'quick','maths','ree','eeee','eeeee','eeeee','eeeee','eeeee','eeeee','eeee','eeee']});
+
+	for (let gif = 0; gif < 20; gif++) {
+ 		gif_promises.push(
+ 			new Promise((resolve, reject) => {
+				giphy.retrieveGIF(giphy.api_key, resolve, reject)
+			})
+		);
+	}
+
+	Promise.all(gif_promises).then(gif_promises => {
+		res.json({ gifs: gif_promises });
+	});
 });
 
 module.exports = router;
